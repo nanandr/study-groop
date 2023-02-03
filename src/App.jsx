27a7e18d4e8e8f5d20cbc9faa1266f26f551fb36
nanandr <rafirenan12@gmail.com>
2023-02-03 14:08:15 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-stone-900 text-7xl text-center'>Study Groop</h1>
+  )
+}
+
+export default App
